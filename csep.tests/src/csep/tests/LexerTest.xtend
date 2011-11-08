3044842a8def4771282471be8c4857e2c7986741
Adam Schmideg <adam@schmideg.net>
2011-11-08 18:57:07 +0100

    Convert beaver symbol to Antlr token @1.7
    --HG--
    branch : aptana

---
+++ csep.tests/src/csep/tests/LexerTest.xtend
@@ -0,0 +1,29 @@
+package csep.tests
+
+import org.junit.runner.RunWith
+import org.eclipse.xtext.junit4.XtextRunner
+import org.eclipse.xtext.junit4.InjectWith
+import com.google.inject.Inject
+import org.junit.Test
+import org.eclipse.xtext.junit4.util.ParseHelper
+import junit.framework.Assert
+import csep.coffeeScript.Model
+import csep.coffeeScript.Greeting
+import java.util.*
+import csep.parser.Lexer
+
+@org.junit.runner.RunWith(typeof(XtextRunner))
+@InjectWith(typeof(InjectorProviderCustom))
+class LexerTest {
+	
+	@Inject
+	ParseHelper<Model> parser
+	
+	@Test
+	def void testSimple() {
+		val lexer = new Lexer("number = 42")
+		val tokens = lexer.tokenize()
+		println("tokens: " + tokens)
+	}
+	
+}
--- csep/src/csep/parser/Lexer.java
+++ csep/src/csep/parser/Lexer.java
@@ -1,28 +1,72 @@
 package csep.parser;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CharStream;
+import org.antlr.runtime.CommonToken;
 import org.antlr.runtime.RecognitionException;
 import org.antlr.runtime.Token;
 
+import beaver.Symbol;
+
+import com.aptana.editor.coffee.parsing.Terminals;
+import com.aptana.editor.coffee.parsing.lexer.CoffeeScanner;
+import com.aptana.editor.coffee.parsing.lexer.CoffeeSymbol;
+
 //public class Lexer extends org.eclipse.xtext.parser.antlr.Lexer {
 public class Lexer extends csep.parser.antlr.internal.InternalCoffeeScriptLexer {
+	private CoffeeScanner aptanaScanner;
 
 	public Lexer(CharStream in) {
 		super(in);
+		aptanaScanner = new CoffeeScanner();
+		String content = in.substring(0, in.size() - 1);
+		aptanaScanner.setSource(content);
+	}
+
+	public Lexer(String str) {
+		this(new ANTLRStringStream(str));
 	}
 
 	@Override
-	public void mTokens() throws RecognitionException {	
-		super.mTokens();
-	}
-	
-	@Override
 	public Token nextToken() {
-		Token token = super.nextToken();
-		if (token.getType() == this.RULE_ID)
-			token.setText("<" + token.getText() + ">");
-		System.out.println("token: " + token.getChannel() + ", " + token.getType() + ": " + token.getText());
+		Token token = null;
+		try {
+			CoffeeSymbol symbol = aptanaScanner.nextToken();
+			if (symbol.getId() == Terminals.EOF) {
+				token = new CommonToken(CommonToken.EOF);
+			}
+			else {
+				String text = "" + symbol.getValue();
+				token = new CommonToken(symbol.getId(), text);
+				int packedStart = symbol.getStart();
+				int startLine = Symbol.getLine(packedStart);
+				int startPosition = Symbol.getColumn(packedStart);
+				token.setLine(startLine);
+				token.setCharPositionInLine(startPosition);
+			} 
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			token = new CommonToken(CommonToken.INVALID_TOKEN); 
+		}
+		System.out.println("token: " + token + ", " + token.getType() + ": " + token.getText());
 		return token;
+	}
+
+	public List<Token> tokenize() throws Exception {
+		List<Token> symbols = new ArrayList<Token>();
+		while (true) {
+			Token token = nextToken();
+			if (Token.EOF == token.getType()) {
+				break;
+			} else {
+				symbols.add(token);
+			}
+		}
+		return symbols;
 	}
 
 }
