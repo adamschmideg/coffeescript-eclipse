1a266461156ece7abaac4fa5e726324ff1f0dbae
Adam Schmideg <adam@schmideg.net>
2012-03-14 23:26:17 +0100

    Create scope with implicit variable @0.5
    --HG--
    branch : implicit-variable

--- example/csep.example.cake/src/csep/example/cake/scoping/CakefileScopeProvider.java
+++ example/csep.example.cake/src/csep/example/cake/scoping/CakefileScopeProvider.java
@@ -7,12 +7,17 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.xtext.linking.impl.LinkingHelper;
 import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.resource.EObjectDescription;
+import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.scoping.IScope;
 import org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider;
+import org.eclipse.xtext.scoping.impl.SingletonScope;
 
 import com.google.inject.Inject;
 
 import csep.coffeeScript.Block;
+import csep.coffeeScript.CoffeeScriptFactory;
+import csep.coffeeScript.Id;
 import csep.example.cake.cakefile.TaskDeclaration;
 
 /**
@@ -27,12 +32,19 @@ public class CakefileScopeProvider extends AbstractDeclarativeScopeProvider impl
 	private LinkingHelper linkingHelper;
 	
 	INode node;
+	Id options;
+	
+	public CakefileScopeProvider() {
+		super();
+		options = CoffeeScriptFactory.eINSTANCE.createId();
+		options.setName("options");
+	}
 	
 	public IScope scope_Id(TaskDeclaration owner, EReference ref) {
 		String crossrefName = linkingHelper.getCrossRefNodeAsString(node, true);
 		if ("options".equals(crossrefName)) {
-			// TODO: create a scope with it
-			return IScope.NULLSCOPE;
+			IEObjectDescription description = EObjectDescription.create(options.getName(), options);
+			return new SingletonScope(description, IScope.NULLSCOPE);
 		}
 		else {
 			// Let superclass handle it
